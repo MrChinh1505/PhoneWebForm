f74eba969cf574eb0e585b9d767185d6d7fc2046
“phanluonghuy” <phanhuy04062003@gmail.com>
2023-05-11 20:16:26 +0700

    20_11_5

--- Controllers/PaymentController.cs
+++ Controllers/PaymentController.cs
@@ -9,6 +9,10 @@ using System.Security.Cryptography;
 using System.Net;
 using System.Text;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Collections.Specialized;
+using Microsoft.AspNetCore.Http;
+using System.Web;
+using Azure;
 
 namespace MobilePhone.Controllers
 {
@@ -175,7 +179,116 @@ namespace MobilePhone.Controllers
                 }
             }
         }
-        
+
+
+        public ActionResult PayVNPay()
+        {
+            List<CartItem> cart = HttpContext.Session.Get<List<CartItem>>("Cart");
+
+            if (cart == null)
+            {
+                ViewBag.Message = "Cart empty";
+                return RedirectToAction("Fail", "Order");
+            }
+
+            else
+            {
+                HttpClient client = new HttpClient();
+
+                //string endpoint = Configuration["Momo:endpoint"];
+                //string partnerCode = Configuration["Momo:partnerCode"];
+                //string accessKey = Configuration["Momo:accessKey"];
+                //string secretKey = Configuration["Momo:secretKey"];
+                //string requestId = "";
+
+                // order information
+                string orderId = Guid.NewGuid().ToString();
+                string amount = (cart.Sum(c => c.price * c.quantity) * ((int)Unit.VND)).ToString() ?? "0";
+
+
+                // rawHash for signature
+                string url = Configuration["VNPay:Url"];
+                string returnUrl = Configuration["VNPay:ReturnUrl"];
+                string tmnCode = Configuration["VNPay:TmnCode"];
+                string hashSecret = Configuration["VNPay:HashSecret"];
+
+                PayLib pay = new PayLib();
+
+                pay.AddRequestData("vnp_Version", "2.1.0"); //Phiên bản api mà merchant kết nối. Phiên bản hiện tại là 2.1.0
+                pay.AddRequestData("vnp_Command", "pay"); //Mã API sử dụng, mã cho giao dịch thanh toán là 'pay'
+                pay.AddRequestData("vnp_TmnCode", tmnCode); //Mã website của merchant trên hệ thống của VNPAY (khi đăng ký tài khoản sẽ có trong mail VNPAY gửi về)
+                pay.AddRequestData("vnp_Amount", amount); //số tiền cần thanh toán, công thức: số tiền * 100 - ví dụ 10.000 (mười nghìn đồng) --> 1000000
+                pay.AddRequestData("vnp_BankCode", ""); //Mã Ngân hàng thanh toán (tham khảo: https://sandbox.vnpayment.vn/apis/danh-sach-ngan-hang/), có thể để trống, người dùng có thể chọn trên cổng thanh toán VNPAY
+                pay.AddRequestData("vnp_CreateDate", DateTime.Now.ToString("yyyyMMddHHmmss")); //ngày thanh toán theo định dạng yyyyMMddHHmmss
+                pay.AddRequestData("vnp_CurrCode", "VND"); //Đơn vị tiền tệ sử dụng thanh toán. Hiện tại chỉ hỗ trợ VND
+                pay.AddRequestData("vnp_IpAddr", Util.GetIpAddress()); //Địa chỉ IP của khách hàng thực hiện giao dịch
+                pay.AddRequestData("vnp_Locale", "en"); //Ngôn ngữ giao diện hiển thị - Tiếng Việt (vn), Tiếng Anh (en)
+                pay.AddRequestData("vnp_OrderInfo", "Thanh toan don hang"); //Thông tin mô tả nội dung thanh toán
+                pay.AddRequestData("vnp_OrderType", "other"); //topup: Nạp tiền điện thoại - billpayment: Thanh toán hóa đơn - fashion: Thời trang - other: Thanh toán trực tuyến
+                pay.AddRequestData("vnp_ReturnUrl", returnUrl); //URL thông báo kết quả giao dịch khi Khách hàng kết thúc thanh toán
+                pay.AddRequestData("vnp_TxnRef", DateTime.Now.Ticks.ToString()); //mã hóa đơn
+
+                string paymentUrl = pay.CreateRequestUrl(url, hashSecret);
+
+                return Redirect(paymentUrl);
+            }
+        }
+        public ActionResult PaymentConfirm()
+        {
+            if (Request.QueryString.HasValue == true)
+            {
+                string hashSecret = Configuration["VNPay:HashSecret"]; //Chuỗi bí mật
+                var vnpayData1 = Request.QueryString;
+                PayLib pay = new PayLib();
+                NameValueCollection vnpayData = HttpUtility.ParseQueryString(vnpayData1.ToString());
+                //lấy toàn bộ dữ liệu được trả về
+                foreach (string s in vnpayData)
+                {
+                    if (!string.IsNullOrEmpty(s) && s.StartsWith("vnp_"))
+                    {
+                        pay.AddResponseData(s, vnpayData[s]);
+                    }
+                }
+
+                long orderId = Convert.ToInt64(pay.GetResponseData("vnp_TxnRef")); //mã hóa đơn
+                long vnpayTranId = Convert.ToInt64(pay.GetResponseData("vnp_TransactionNo")); //mã giao dịch tại hệ thống VNPAY
+                string vnp_ResponseCode = pay.GetResponseData("vnp_ResponseCode"); //response code: 00 - thành công, khác 00 - xem thêm https://sandbox.vnpayment.vn/apis/docs/bang-ma-loi/
+                //string vnp_SecureHash = Request.QueryString["vnp_SecureHash"]; //hash của dữ liệu trả về
+                string vnp_SecureHash = Request.Query["vnp_SecureHash"];
+
+                bool checkSignature = pay.ValidateSignature(vnp_SecureHash, hashSecret); //check chữ ký đúng hay không?
+
+                string amount = Convert.ToString(pay.GetResponseData("vnp_Amount"));
+
+                if (checkSignature)
+                {
+                    if (vnp_ResponseCode == "00")
+                    {
+                        //Thanh toán thành công
+                        ViewBag.Message = "Thanh toán thành công hóa đơn " + orderId + " | Mã giao dịch: " + vnpayTranId;
+                        return RedirectToAction("Success", "Order"); ;
+                    }
+                    else
+                    {
+                        //Thanh toán không thành công. Mã lỗi: vnp_ResponseCode
+                        ViewBag.Message = "Có lỗi xảy ra trong quá trình xử lý hóa đơn " + orderId + " | Mã giao dịch: " + vnpayTranId + " | Mã lỗi: " + vnp_ResponseCode;
+                        return RedirectToAction("Fail", "Order");
+                    }
+                }
+                else
+                {
+
+                    return RedirectToAction("Fail", "Order");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Fail", "Order");
+            }
+
+            return View();
+        }
+
         // remember that you will be verify with private key when processing status 
 
         public ActionResult ReturnUrl()
---
+++ Models/PayCompare.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace MobilePhone.Models
+{
+    public class PayCompare : IComparer<string>
+
+    {
+
+        public int Compare(string x, string y)
+
+        {
+
+            if (x == y) return 0;
+
+            if (x == null) return -1;
+
+            if (y == null) return 1;
+
+            var Compare = CompareInfo.GetCompareInfo("en-US");
+
+            return Compare.Compare(x, y, CompareOptions.Ordinal);
+
+        }
+
+    }
+}
